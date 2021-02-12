f560f62a0b04e917f8d2c0294ac25f85283ea7c6
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2021-02-12 10:29:25 -0500

    fixed misc spelling errors

--- noc.js
+++ noc.js
@@ -16,7 +16,7 @@ program
   .option('-c, --exec-command <command>', 'specify command to run (e.g. firefox.exe ringtone.mp3)', null )
   // .option('-l, --local-time <number>', 'adjust date time to local time (e.g. "+9" for Japan)', 9) // no needed to adjust to local time
   .option('-l, --log <text>', 'specify console log message', "Notified.")
-  .option('-H, --hide-log', 'hide infomation logs')
+  .option('-H, --hide-log', 'hide information logs')
   .parse(process.argv)
   ;
   if (! process.argv.slice(2).length) program.help() ;
